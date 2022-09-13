29222e5f23786cbc0e0ee5b12a7c86aa315491c5
Viseko <viseko23@gmail.com>
2022-09-13 19:55:43 +0700

    Added table section layout

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,23 @@
 // jQuery
 import $ from "jquery";
+
+// Data
+import goodsData from "./data/goods.js";
+
+// Fetching goods data and build table
+const $table = $(".table");
+
+const $tableRows = goodsData.map((data) => {
+  // Cloning HTML-template
+  const $rowBlock = $($("#js-table-row-block").html().trim());
+  $rowBlock.find("[data-field=name]").html(data.name);
+  $rowBlock.find("[data-field=material]").html(data.material);
+  $rowBlock
+    .find("[data-field=sizes]")
+    .html(`${data.sizes.join(" × ")}&nbsp;мм`);
+  $rowBlock.find("[data-field=price]").html(`${data.price}&nbsp;₽`);
+
+  return $rowBlock;
+});
+
+$table.append($tableRows);
---
+++ src/js/data/goods.js
@@ -0,0 +1,32 @@
+const goods = [
+  {
+    id: 0,
+    name: "Стеновой паркет",
+    material: "Ольха",
+    sizes: [13, 78, 300],
+    price: 52,
+  },
+  {
+    id: 1,
+    name: "Стеновой паркет",
+    material: "Ольха",
+    sizes: [13, 78, 600],
+    price: 112,
+  },
+  {
+    id: 2,
+    name: "Доска для пола, сорт А",
+    material: "Лиственница",
+    sizes: [27, 134, 2000],
+    price: 1062,
+  },
+  {
+    id: 3,
+    name: "Вагонка",
+    material: "Термо-абаш",
+    sizes: [19, 90, 2000],
+    price: 1009,
+  },
+];
+
+export default goods;
